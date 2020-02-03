f562507dff7901515900c1e06d28fc76e4c28533
Nicholas Gan <nicholasganshyan@gmail.com>
2020-02-03 20:36:27 +0800

    add Jetson CAN test Node

--- TestBench/Teensy_CAN_Node/Teensy_CAN_Node.ino
+++ TestBench/Teensy_CAN_Node/Teensy_CAN_Node.ino
@@ -1,40 +1,35 @@
-/*
- Name:		Teensy_CAN_Node.ino
- Created:	1/29/2020 8:47:57 PM
- Author:	Nicho
-*/
-
-// Teensy Node for testbench the CAN bus with the Teensy
-
 #include <FlexCAN.h>
 
-void hexDump(uint8_t len, uint8_t* buf) {
-	for (uint8_t i = 0; i < 8; i++) {
-		Serial.print(buf[i]); Serial.print(" ,");
+void setup() {
+	delay(2000);
+	Serial.println(F("Hello Teensy 3.5 CAN Test."));
+	Can0.begin(1000000);
+}
+
+void loop() {
+	CAN_message_t inMsg;
+	inMsg.id = 0x100;
+  if(Can0.available()){
+    Serial.println("Message available");
+  } else {
+    Serial.println("Not available");
+  }
+  Can0.read(inMsg); 
+  Serial.println(inMsg.id);
+	while (Can0.available()) {
+		Serial.println("Reading in message...");
+		Can0.read(inMsg);
+		printMsg(inMsg);
+	}
+	Serial.println("End of loop...");
+}
+
+void printMsg(CAN_message_t& inMsg) {
+	Serial.print("ID: "); Serial.println(inMsg.id);
+	Serial.print("Length: "); Serial.println(inMsg.len);
+	Serial.print("Data: "); 
+	for (int i = 0; i < inMsg.len; i++) {
+		Serial.print(inMsg.buf[i]);
 	}
 	Serial.println("\n");
-}
-
-// the setup function runs once when you press reset or power the board
-void setup() {
-	delay(1000);
-	Serial.begin(9600);
-	Can0.begin(1000000);
-	delay(500);
-}
-
-// the loop function runs over and over again until power down or reset
-void loop() {
-
-	// Polling
-	CAN_message_t inMsg;
-	while (Can0.available()) {
-		Can0.read(inMsg);
-		Serial.println("CAN message received ... ");
-		Serial.print("ID: "); Serial.println(inMsg.id);
-		Serial.print("Length: "); Serial.println(inMsg.len);
-		hexDump(inMsg.len, inMsg.buf);
-	}
-	Serial.println("End of loop");
-	delay(20);
 }
